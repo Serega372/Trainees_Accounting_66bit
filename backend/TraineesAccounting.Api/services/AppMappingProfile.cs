2ea4c299c12f492c2bc7835c1f072803d2342b04
Serega <sergey.123321123@mail.ru>
2025-02-23 15:42:31 +0500

    create mapping of dto in trainee entity

---
+++ backend/TraineesAccounting.Api/services/AppMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using TraineesAccounting.Api.Dtos;
+using TraineesAccounting.Persistence.Entities;
+
+namespace TraineesAccounting.Api.Services
+{
+    public class AppMappingProfile : Profile
+    {
+        public AppMappingProfile()
+        {
+            CreateMap<TraineesResponse, TraineeEntity>();
+            CreateMap<AddTraineeRequest,  TraineeEntity>();
+            CreateMap<UpdateTraineeRequest, TraineeEntity>();
+            CreateMap<TraineeEntity, TraineesResponse>();
+        }
+    }
+}
