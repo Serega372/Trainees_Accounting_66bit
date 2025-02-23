11a46e01624ca19959b9ab1d86b532f02ababc42
Serega <sergey.123321123@mail.ru>
2025-02-23 15:30:19 +0500

    add dto for trainee

---
+++ backend/TraineesAccounting.Api/Dtos/TraineeDto.cs
@@ -0,0 +1,35 @@
+﻿namespace TraineesAccounting.Api.Dtos
+{
+    public record TraineesResponse(
+        Guid Id,
+        string Name,
+        string Surname,
+        string Gender,
+        string Email,
+        string? PhoneNumber,
+        DateOnly Birthday,
+        string? InternshipTitle,
+        string? ProjectTitle,
+        Guid? InternshipDirectionId,
+        Guid? ProjectId);
+
+    public record AddTraineeRequest(
+        string Name,
+        string Surname,
+        string Gender,
+        string Email,
+        string? PhoneNumber,
+        DateOnly Birtday,
+        Guid InternshipDirectionId,
+        Guid ProjectId);
+
+    public record UpdateTraineeRequest(
+        string Name,
+        string Surname,
+        string Gender,
+        string Email,
+        string? PhoneNumber,
+        DateOnly Birtday,
+        Guid InternshipDirectionId,
+        Guid ProjectId);
+}
