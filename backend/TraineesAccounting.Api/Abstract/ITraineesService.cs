5bcd6b95b619a5ff5cf34e57df84c47e65b1ab9f
Serega <sergey.123321123@mail.ru>
2025-02-23 17:18:45 +0500

    add trainees interface

---
+++ backend/TraineesAccounting.Api/Abstract/ITraineesService.cs
@@ -0,0 +1,14 @@
+﻿using TraineesAccounting.Api.Dtos;
+
+namespace TraineesAccounting.Api.Abstract
+{
+    public interface ITraineesService
+    {
+        Task Add(AddTraineeRequest traineeDto);
+        Task<List<TraineesResponse>> All();
+        Task Delete(Guid id);
+        Task<TraineesResponse> GetById(Guid id);
+        Task<List<TraineesResponse>> GetByPage(int page, int pageSize);
+        Task Update(Guid id, UpdateTraineeRequest updatedTraineeDto);
+    }
+}
