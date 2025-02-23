22e942bd1667ec26d61a7a8eec554dcfffc52eed
Serega <sergey.123321123@mail.ru>
2025-02-23 13:51:25 +0500

    add database context and register it

---
+++ backend/TraineesAccounting.Persistence/DatabaseContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TraineesAccounting.Persistence.Entities;
+
+namespace TraineesAccounting.Persistence
+{
+    public class DatabaseContext(
+        DbContextOptions<DatabaseContext> options
+        ) : DbContext(options)
+    {
+        public DbSet<TraineeEntity> Trainees { get; set; }
+        public DbSet<InternshipDirectionEntity> InternshipDirections { get; set; }
+        public DbSet<ProjectEntity> Projects { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TraineeEntity>()
+                .HasOne(t => t.InternshipDirection)
+                .WithMany(i => i.Trainees)
+                .HasForeignKey(t => t.InternshipDirectionId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            modelBuilder.Entity<TraineeEntity>()
+                .HasOne(t => t.Project)
+                .WithMany(p => p.Trainees)
+                .HasForeignKey(t => t.ProjectId)
+                .OnDelete(DeleteBehavior.SetNull);
+        }
+    }
+}
