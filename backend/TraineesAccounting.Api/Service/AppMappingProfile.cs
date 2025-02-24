2d2b9c1e11b292717797eb988c310dffa1713584
Serega <sergey.123321123@mail.ru>
2025-02-24 21:20:20 +0500

    test fix

---
+++ backend/TraineesAccounting.Api/Service/AppMappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using TraineesAccounting.Api.Dtos;
+using TraineesAccounting.Persistence.Entities;
+
+namespace TraineesAccounting.Api.Services
+{
+    public class AppMappingProfile : Profile
+    {
+        public AppMappingProfile()
+        {
+            CreateMap<TraineesResponse, TraineeEntity>();
+            CreateMap<AddTraineeRequest,  TraineeEntity>();
+            CreateMap<UpdateTraineeRequest, TraineeEntity>();
+            CreateMap<TraineeEntity, TraineesResponse>();
+
+            CreateMap<InternshipDirectionsResponse, InternshipDirectionEntity>();
+            CreateMap<AddInternshipDirectionRequest, InternshipDirectionEntity>();
+            CreateMap<UpdateInternshipDirectionRequest, InternshipDirectionEntity>();
+            CreateMap<InternshipDirectionEntity, InternshipDirectionsResponse>();
+        }
+    }
+}
---
+++ backend/TraineesAccounting.Api/services/InternshipDirectionsService.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using TraineesAccounting.Api.Dtos;
+using TraineesAccounting.Persistence.Abstract;
+
+namespace TraineesAccounting.Api.Services
+{
+    public class InternshipDirectionsService(
+        IInternshipDirectionsRepository internshipDirectionsRepository,
+        IMapper mapper)
+    {
+        public async Task<List<InternshipDirectionsResponse>> All()
+        {
+
+        }
+    }
+}
