f6ab8ad6cdb4f96ce6d71b7f93b269e43bdaa8a0
Serega <sergey.123321123@mail.ru>
2025-02-25 14:14:53 +0500

    add dto for project

---
+++ backend/TraineesAccounting.Api/Dtos/ProjectDto.cs
@@ -0,0 +1,12 @@
+﻿namespace TraineesAccounting.Api.Dtos
+{
+    public record ProjectsResponse(
+        Guid Id,
+        string ProjectTitle);
+
+    public record AddProjectRequest(
+        string ProjectTitle);
+
+    public record UpdateProjectRequest(
+        string ProjectTitle);
+}
